9257db525a3aee11b3966eff4433fbb29a78c3b9
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-07 21:50:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Create a react app from scratch.
+const React = require("react");
+const ReactDOM = require("react-dom");
+//It should display 2 paragraph HTML elements.
+const name = "Ajay Pradhan";
+const year = "2021";
+ReactDOM.render(
+  <div>
+    <h1>Created by {name}</h1>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
